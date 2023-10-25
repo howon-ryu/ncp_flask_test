91b64bff46569865bef836b48d24a02ad4d747da
howon-ryu <qorgh2akfl@naver.com>
2023-10-25 04:31:10 +0000

    translate

--- start.py
+++ start.py
@@ -1,5 +1,8 @@
 from flask import Flask, Response, request
 from flask_cors import CORS  
+from transformers import pipeline
+import gradio as gr
+
 
 app = Flask(__name__)
 host_addr = "0.0.0.0"
@@ -27,6 +30,20 @@ def ping():
     response.headers['Access-Control-Allow-Origin'] = '*'
     #response.headers['Access-Control-Allow-Origin'] = '*'
     return response
+@app.route('/translate', methods=['POST'])
+def translate():
+    pipe = pipeline("text2text-generation", model="inhee/opus-mt-ko-en-finetuned-ko-to-en5")
+    input_data = request.get_json()
+    response_data = input_data["data"]
+    return_response = pipe(response_data)
 
+    response = Response(return_response[0]['generated_text'], content_type="application/json")
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    
+    return response
 if __name__ == "__main__":
     app.run(debug=True, host=host_addr, port=host_port)
+
+
+    
+    
